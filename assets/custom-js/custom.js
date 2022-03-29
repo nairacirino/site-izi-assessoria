a5596c6c26a81ca42341bb070267a642ce9044e9
Naira Cirino <nacirino@gmail.com>
2022-03-29 16:26:55 -0300

    Atualizações na home e na página de projetos
    Na home fiz a inclusão das imagens enviadas
    
    Na página de projetos:
    
    - Alterei as imagens do projeto 'fotografia'
    - Fiz as animações de transição da página
    - Acrescentei as copys encaminhadas

--- assets/custom-js/custom.js
+++ assets/custom-js/custom.js
@@ -1,26 +1,59 @@
+// //função substitui imagem
+// function subImagem(x) {
+//     document.getElementById(x).src = "./assets/media/img/projetos/fotografia/Projetos-fotografia-ladoesquerda-imagem02.jpg";
+// }
+
+// funções para hover preto e branco x colorido //
 function pbImg(x) {
-    x.style.transition= "2s";
+    x.style.transition = "2s";
     x.style.filter = "grayscale(100%)";
 }
 
 function colorImg(x) {
-    x.style.transition= "2s";
+    x.style.transition = "2s";
     x.style.filter = "grayscale(0%)";
 }
 
-window.onscroll = function() {scrollFunction()};
+// diminuição de margem superior na barra de navegação //
+
+window.onscroll = function () { scrollFunction() };
 
 function scrollFunction() {
     if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
-        document.getElementById("navbar").style.padding="0px";
-        document.getElementById("logo-expandido").style.padding="0px";
+        document.getElementById("navbar").style.padding = "0px";
+        document.getElementById("logo-expandido").style.padding = "0px";
     } else {
-        document.getElementById("navbar").style.padding="1.5rem 0 0 0";
-        document.getElementById("logo-expandido").style.padding="0 0 1.5rem 0";
+        document.getElementById("navbar").style.padding = "1.5rem 0 0 0";
+        document.getElementById("logo-expandido").style.padding = "0 0 1.5rem 0";
     }
-    console.log("isso funciona");
-    };
+};
 
-    function ctaAparece(x) {
-        x.style.display = "block";
+// link de cta aparecendo em hover //
+
+function ctaAparece(x) {
+    x.style.display = "block";
+}
+
+// slideshow imagens em projetos //
+
+function addClass(id, classe) {
+    var elemento = document.getElementById(id);
+    var classes = elemento.className.split(' ');
+    var getIndex = classes.indexOf(classe);
+
+    if (getIndex === -1) {
+        classes.push(classe);
+        elemento.className = classes.join(' ');
     }
+}
+
+function delClass(id, classe) {
+    var elemento = document.getElementById(id);
+    var classes = elemento.className.split(' ');
+    var getIndex = classes.indexOf(classe);
+
+    if (getIndex > -1) {
+        classes.splice(getIndex, 1);
+    }
+    elemento.className = classes.join(' ');
+}
